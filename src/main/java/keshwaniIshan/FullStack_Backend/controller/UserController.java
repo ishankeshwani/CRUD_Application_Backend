382cf479d9c3b393445b2099ea71c3f6754a8a02
Ishan Keshwani <keshwaniishan789@gmail.com>
2026-06-18 20:26:00 +0530

    Update CORS for deployment

--- src/main/java/keshwaniIshan/FullStack_Backend/controller/UserController.java
+++ src/main/java/keshwaniIshan/FullStack_Backend/controller/UserController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin(origins = {
+        "http://localhost:3000",
+        "https://YOUR-NETLIFY-SITE.netlify.app"
+})
 public class UserController {
 
     @Autowired
